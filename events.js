f3725d3f4148ee896aec9bc5be88c6ebde8a0d37
BryanOri <bryanorihuela@computingforall.org>
2021-10-15 14:37:21 -0800

    stuff

--- events.js
+++ events.js
@@ -11,19 +11,34 @@ function initialize()
 {
   // 1) Convert the "onkeyup" and "onkeydown" event attributes attached to the
   //    body HTML element into JavaScript "addEventListener" calls.
-
+  let body = document.body
+  body.removeAttribute("onkeydown")
+  body.removeAttribute("onkeyup")
+  document.addEventListener("keydown", handleOnKeyDown)
+  document.addEventListener("keyup", handleOnKeyUp)
 
   // 2) Convert the "onchange" event attribute attached to the input with id
   //    "inputBox" into a JavaScript "addEventListener" call.
-
+  let box = document.querySelector("#inputBox")
+  box.removeAttribute("onchange")
+  box.addEventListener("change", handleInputChanged) 
 
   // 3) Convert the "onmouseover" and "onmouseout" event attributes attached
   //    to the textarea with id "log" into JavaScript "addEventListener" calls.
-
-
+  let log = document.querySelector("#log")
+  log.removeAttribute("onmouseover")
+  log.removeAttribute("onmouseout")
+  log.addEventListener("mouseover", function(){
+    changeBgColor('gray')
+  })
+  log.addEventListener("mouseout", function(){
+    changeBgColor('white')
+  })
   // 4) Convert the "onclick" event attribute attached to the button with id
   //    "clearButton" into a JavaScript "addEventListener" call.
-
+  let button = document.querySelector("#clearButton")
+  button.removeAttribute("onclick")
+  button.addEventListener("click", clearLog)
 
 }
 
@@ -34,7 +49,7 @@ function initialize()
 //    Note: The counterpart event type to use is "DOMContentLoaded", and
 //          you must apply this "addEventListener" to the "document" object
 //          or the "window" object, not the body DOM element
-
+document.addEventListener("DOMContentloaded", initialize)
 
 //////////////////////////////////////
 //// Do not modify anything below ////
